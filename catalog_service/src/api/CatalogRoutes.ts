8d2f5fa8508fa9e98d7d0d83e45e901317a2cdb0
Zohaib Mir <zohaib.mir@vaimo.com>
2024-11-03 00:41:41 +0100

    Test End points

--- catalog_service/src/api/CatalogRoutes.ts
+++ catalog_service/src/api/CatalogRoutes.ts
@@ -1,11 +1,16 @@
 import express, { NextFunction, Request, Response } from 'express';
+import { CatalogService } from '../services/catalogService';
+import { CatalogRepository } from '../repository/catalogRepository';
 
 const router = express.Router();
 
+const catalogService = new CatalogService(new CatalogRepository());
+
 router.post(
-    "/product", 
+    "/products", 
     async (req: Request, res: Response, next: NextFunction) => {
-       return next( res.status(201).json({}));
+        const data = await catalogService.createProduct(req.body);
+       return next( res.status(201).json(data));
     }
 );
 

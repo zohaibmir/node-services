c31f918a7629411199a647c242b2d39bae91974d
Zohaib Mir <zohaib.mir@vaimo.com>
2024-11-03 16:26:08 +0100

    connect cart with db

--- order_service/src/repository/cartRepository.ts
+++ order_service/src/repository/cartRepository.ts
@@ -1,9 +1,14 @@
 import { CartRepositoryType } from "../../types/Repository";
+import { DB } from "../db/dbConnection";
+import { Cart, CartLineItem, cartLineItems, carts } from "../db/schema";
 
 
 const createCart = async (input: any): Promise<{}> => {
+    const result = await DB.insert(carts).values( {
+        customerId:124
+    }).returning({cartId: carts.id});
 
-    return Promise.resolve({message: "Test response from cart repository", input});
+    return Promise.resolve({message: result, input});
 }
 
 const updateCart = async (input: any): Promise<{}> => {

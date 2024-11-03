3cad24211039ccef3fff07af7a98c9f096d73201
Zohaib Mir <zohaib.mir@vaimo.com>
2024-11-03 01:33:12 +0100

    Add Product validator

--- catalog_service/src/api/CatalogRoutes.ts
+++ catalog_service/src/api/CatalogRoutes.ts
@@ -1,6 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { CatalogService } from '../services/catalogService';
 import { CatalogRepository } from '../repository/catalogRepository';
+import { RequestValidator } from '../utils/requestValidator';
+import { ProductRequest } from '../dto/ProductRequest';
+import { json } from 'stream/consumers';
 
 const router = express.Router();
 
@@ -9,8 +12,14 @@ export const catalogService = new CatalogService(new CatalogRepository());
 router.post(
     "/products", 
     async (req: Request, res: Response, next: NextFunction) => {
+        const { errors, input } = await RequestValidator(ProductRequest, req.body);
+        if (errors) {
+            
+            return  next(res.status(400).json(errors));
+        }
         const data = await catalogService.createProduct(req.body);
-       return next( res.status(201).json(data));
+        
+        return next( res.status(201).json(data));
     }
 );
 
